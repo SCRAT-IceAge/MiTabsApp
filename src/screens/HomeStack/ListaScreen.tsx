ce68ffbfb8082300c99cfa9f4eac61e618d1cf81
Pablo <pablo.cau15@gmail.com>
2026-05-08 18:02:33 -0300

    0.4.1 Decido por la tematica de 'Personajes de The Office. Cambio titulo, 'Botones', y agrego imagenes

--- src/screens/HomeStack/ListaScreen.tsx
+++ src/screens/HomeStack/ListaScreen.tsx
@@ -3,44 +3,57 @@ Agrego una lista (visto al final de la clase 3, 1:25)
 */
 
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet , Image} from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { HomeStackParamList } from '../../types/navigation';
 
 type Props = NativeStackScreenProps<HomeStackParamList, 'Lista'>;
 
 const items = [
-  { id: 1, nombre: 'Item 1' },
-  { id: 2, nombre: 'Item 2' },
-  { id: 3, nombre: 'Item 3' },
+  { id: 1, nombre: 'Jim' },
+  { id: 2, nombre: 'Pam' },
+  { id: 3, nombre: 'Michael' },
 ];
 
 export default function ListaScreen({ navigation }: Props) {
   return (
-    <ImageBackground
-      source={require('../../../assets/steve.jpg')}
-      style={styles.background}
-    >
-      <View style={styles.container}>
-        <Text style={styles.titulo}>Lista</Text>
-        {items.map((item) => (
-          <TouchableOpacity
-            key={item.id}
-            style={styles.item}
-            onPress={() => navigation.navigate('Detalle', { id: item.id })}
-          >
-            <Text style={styles.itemTexto}>{item.nombre}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    </ImageBackground>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Personajes de The Office</Text>
+      {items.map((item) => (
+        <TouchableOpacity
+          key={item.id}
+          style={styles.item}
+          onPress={() => navigation.navigate('Detalle', { id: item.id })}
+        >
+          {item.id === 1 && (
+            <Image
+              source={require('../../../assets/jimlista.jpg')}
+              style={styles.imagen}
+            />
+          )}
+                    {item.id === 2 && (
+            <Image
+              source={require('../../../assets/pamlista.jpg')}
+              style={styles.imagen}
+            />
+          )}
+                    {item.id === 3 && (
+            <Image
+              source={require('../../../assets/michaellista.jpg')}
+              style={styles.imagen}
+            />
+          )}
+          <Text style={styles.itemTexto}>{item.nombre}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  background: { flex: 1 },
-  container: { flex: 1, padding: 24, paddingTop: 60 },
-  titulo: { fontSize: 32, fontWeight: 'bold', color: '#fff', marginBottom: 24 },
-  item: { backgroundColor: '#fff', padding: 16, borderRadius: 12, marginBottom: 12, opacity: 0.5 },
-  itemTexto: { fontSize: 16, color: '#1e3a8a' },
+  container: { flex: 1, backgroundColor: '#eff6ff', padding: 24, paddingTop: 60 },
+  titulo: { fontSize: 32, fontWeight: 'bold', color: '#1e3a8a', marginBottom: 24 },
+  item: { backgroundColor: '#fff', padding: 16, borderRadius: 12, marginBottom: 12, flexDirection: 'row'  },
+  itemTexto: { fontSize: 45, color: '#1e3a8a', paddingLeft:20 },
+  imagen: { width: 70, height: 70, borderRadius: 4 },
 });
---
+++ src/screens/InicioScreen.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet , Image} from 'react-native';
+
+export default function InicioScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Inicio</Text>
+      <Text style={styles.descripcion}>Hola Profe!!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#eff6ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#1e3a8a',
+  },
+  descripcion: {
+    fontSize: 16,
+    color: '#4b5563',
+  },
+});
